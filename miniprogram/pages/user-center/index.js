6ea3ab4db94a0957212616a02caa7bf1c93799bf
Booker Zhao <bookerzhao@tencent.com>
2024-07-06 14:53:43 +0800

    feat: 增加行程列表

--- miniprogram/pages/user-center/index.js
+++ miniprogram/pages/user-center/index.js
@@ -8,6 +8,28 @@ Page({
   data: {
     openId: '',
     showUploadTip: false,
+    itinerary: [
+        // 这里可以是从服务器获取的行程列表数据
+        // 示例数据结构：
+        {
+          destination: '深圳',
+          start_date: '2023-12-01'
+        },
+        // ...
+      ]
+  },
+
+  onLoad: function() {
+    // 这里可以加载行程列表数据
+    // 例如，从服务器请求数据或从本地JSON文件读取
+  },
+  goToDetail: function(e) {
+    // 点击行程列表项跳转到详细行程页面
+    const _id = e.currentTarget.dataset.id;
+
+    wx.navigateTo({
+      url: `/pages/itinerary/index?_id=${_id}`
+    });
   },
 
   getOpenId() {
